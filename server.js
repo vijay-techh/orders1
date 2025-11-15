437c9f625ad3e3b5b45071abec37438528762258
Vijay v <vijayvijaayyyyy@gmail.com>
2025-11-15 12:18:51 +0530

    changed database name

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("docs"));
 
 const PORT = process.env.PORT || 3000;
 
